6c8a22dddbd6599448631cd387df42bdaab15da5
Shubham Kashyap <shubham.kashyap.jha07@gmail.com>
2025-11-28 20:53:59 +0530

    implement product creation with Cloudinary image upload

--- E-commerce-store/backend/controllers/product.controller.js
+++ E-commerce-store/backend/controllers/product.controller.js
@@ -1,7 +1,9 @@
 import productModel from "../models/product.model.js";
+import cloudinary from "../lib/cloudinary.js";
+
 // @desc to get all Products
-// @path /api/products/protectRoute/adminRoute/getAllProducts
-// @access protected
+// @path /api/products/getAllProducts
+// @access protected /api/products/protectRoute/adminRoute
 export const getAllProducts = async (req, res) => {
     try {
         const Products = await productModel.find();
@@ -13,7 +15,7 @@ export const getAllProducts = async (req, res) => {
     
 }
 
-// @desc to get Featured Products
+// @desc to get Featured Products           
 // @path /api/products/featured
 // @access public 
 export const getFeaturedProducts = async (req, res) => {
@@ -42,6 +44,31 @@ export const getFeaturedProducts = async (req, res) => {
     }
 }
 
-// export const createProduct = (req, res) => {
+// @desc admin can create Products
+// @path /api/products/createProduct
+// access protected /api/products/protectRoute/adminRoute
+export const createProduct = async (req, res) => {
+    try {
+        const {name, description, price, image, category} = req.body;
 
-// }
+        let cloudinaryResponse = null;
+
+        if(image){
+            cloudinaryResponse = await cloudinary.uploader.upload(image, {folder: "products"});
+        }
+
+        const product = await productModel.create({
+            name,
+            description,
+            price, 
+            image: cloudinaryResponse?.secure_url ? cloudinaryResponse.secure_url : "",
+            category,
+        })
+
+        res.status(201).json(product);
+    } catch (error) {
+        console.log("Error in createProduct controller");
+        res.status(500).json({message: "Server Error", error: error.message})
+        
+    }
+}
