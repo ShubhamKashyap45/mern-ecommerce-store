5a78f6e7b40ec7dd8de184aa5d11ec165a81b13c
Shubham Kashyap <shubham.kashyap.jha07@gmail.com>
2025-12-02 17:10:22 +0530

    implement removeAllFromCart controller

--- E-commerce-store/backend/routes/cart.route.js
+++ E-commerce-store/backend/routes/cart.route.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { addToCart } from '../controllers/cart.controller.js';
+import { addToCart, removeAllFromCart } from '../controllers/cart.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
 
 router.post('/', protectRoute, addToCart);
+router.delete('/', protectRoute, removeAllFromCart);
 
 export default router;
