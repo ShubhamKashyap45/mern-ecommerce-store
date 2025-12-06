274d67ccc5a6f2e475d7e5bb548575659cb7c972
Shubham Kashyap <shubham.kashyap.jha07@gmail.com>
2025-12-06 14:21:17 +0530

    implement validateCoupon controller with expiration and active status check

--- E-commerce-store/backend/controllers/coupon.controller.js
+++ E-commerce-store/backend/controllers/coupon.controller.js
@@ -12,3 +12,33 @@ export const getCoupon = async (req, res) => {
         res.status(500).json({message: "Server Error", error: error.message});
     }
 }
+
+// desc to validate the Coupon code entered by the user
+// route GET /api/coupons/validate
+// access Protected
+export const validateCoupon = async (req, res) => {
+    try {
+        const {couponcode} = req.body;
+        const coupon = await couponModel.findOne({code: couponcode, userId: req.user._id, isActive: true});
+
+        if(!coupon){
+            return res.status(404).json({message: "Coupon not found"});
+        }
+
+        if(coupon.expirationDate < new Date()){
+            coupon.isActive = false;
+            await coupon.save();
+            return res.status(404).json({message: "Coupon expired"});
+        }
+
+        res.json({
+            message: "Coupon is valid",
+            code: coupon.code,
+            discountPercentage: coupon.discountPercentage,
+        })
+    } catch (error) {
+        console.log("Error in validateCoupon Controller", error.message);
+        res.status(500).json({message: "Server Error", error: error.message})
+        
+    }
+}
