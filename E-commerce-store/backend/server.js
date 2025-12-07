3e93a0f0b916bf54de936904743bafa7eff9ee3d
Shubham Kashyap <shubham.kashyap.jha07@gmail.com>
2025-12-07 18:08:25 +0530

    setup Stripe integration and implement checkout session function

--- E-commerce-store/backend/server.js
+++ E-commerce-store/backend/server.js
@@ -7,6 +7,7 @@ import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js"
 import cartRoutes from "./routes/cart.route.js"
 import couponRoutes from "./routes/coupon.route.js"
+import paymentRoutes from "./routes/payment.route.js"
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 
@@ -21,10 +22,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.get('/', (req, res) => {
     console.log("Welcome to Home Page");
-    res.send("Welcome to Home Page")
+    res.send("Welcome to Home Page");
     
 })
 app.listen(PORT, () => {
